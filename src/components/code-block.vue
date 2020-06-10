adc6cb87ee48596d25a92e20a65a26be90ec326c
Vinay Pillai <vinaypillai01@gmail.com>
2020-06-09 17:11:23 -0700

    feat(code-block.vue): added clipboard
    Added vue fontawesome and copy to clipboard

--- src/components/code-block.vue
+++ src/components/code-block.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <vue-code-highlight :language="language" class="code-block">{{content}}</vue-code-highlight>
+        <div class="copy-button" @click="copy()"><font-awesome-icon icon="copy" /></div>
     </div>
 </template>
 <script type="text/javascript">
@@ -24,6 +25,11 @@
 
             }
         },
+        methods:{
+            copy(){
+                navigator.clipboard.writeText(this.content);
+            }
+        },
         components:{
             VueCodeHighlight,
         }
@@ -36,12 +42,51 @@
         position: relative;
     }
     code{
-
         max-height: 50vh;
-        overflow-y: auto;
+        overflow: auto;
         display: block;
         padding-right: 30px;
         box-sizing: border-box;
         width: 100%;
+        max-width: 90vw;
+    }
+    .copy-button{
+        color: #FFF;
+        font-size: 1.5em;
+        width: 2em;
+        height: 2em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        border: solid 2px #FFF;
+        position: relative;
+        margin: 1em auto -1em auto;
+        cursor: pointer;
+        transition: background-color 1s ease-in-out;
+        &:active{
+            background-color: #FFF;
+            transition: background-color 0.05s ease-in-out;
+            color: #333;
+        }
+        &::after{
+            content:'';
+            position: absolute;
+            top: -3px;
+            bottom: -3px;
+            left: -3px;
+            right: -3px;
+            border: solid 5px #FFF;
+            transition: top,right,bottom,left, 0.15s ease-in-out;
+            border-radius: 50%;
+        }
+        &:hover{
+            &::after{
+                top: -6px;
+                bottom: -6px;
+                left: -6px;
+                right: -6px;
+            }
+        }
     }
 </style>
